767b764278be3777516405662f5bd07d2ab4334f
Ramalakshmi S <ramalakshmidon94@gmail.com>
2026-08-06 22:30:27 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
+from config import *
+from data_loader import *
+from enrichment import *
+from excel_writer import *
+from seller_loader import *
+from merge_data import *
+from violation_detector import *
+from report_generator import *
+from email_generator import *
 
+def main():
+    ...
+    # Stage 1
+    # Stage 2
+    # Stage 3
+    # Stage 4
+    # Stage 5
+    # Stage 6
+    # Stage 7
+
+if __name__ == "__main__":
+    main()
